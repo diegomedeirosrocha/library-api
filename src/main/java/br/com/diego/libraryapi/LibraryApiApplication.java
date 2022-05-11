fdf1328c0cd887fff32d9a33adb0130902670d6d
Diego Rocha <di.rocha27@gmail.com>
2022-05-11 01:56:55 -0300

    chore: LibraryApiApplication carregando massa de dados, load aplicacao

--- src/main/java/br/com/diego/libraryapi/LibraryApiApplication.java
+++ src/main/java/br/com/diego/libraryapi/LibraryApiApplication.java
@@ -1,7 +1,11 @@
 package br.com.diego.libraryapi;
 
+import br.com.diego.libraryapi.models.Book;
+import br.com.diego.libraryapi.repository.BookRepository;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class LibraryApiApplication {
@@ -10,4 +14,13 @@ public class LibraryApiApplication {
         SpringApplication.run(LibraryApiApplication.class, args);
     }
 
+    @Bean
+    public CommandLineRunner commandLineRunner(BookRepository bookRepository) {
+        return args -> {
+            bookRepository.save(Book.builder().id(1L).name("Codigo Limpo").year(2007).status("NEW").build());
+            bookRepository.save(Book.builder().id(2L).name("Lessons Learning").year(2000).status("USED").build());
+            bookRepository.save(Book.builder().id(3L).name("Jornada Java").year(2020).status("NEW").build());
+            bookRepository.save(Book.builder().id(4L).name("Developing Testing").year(2017).status("NEW").build());
+        };
+    }
 }
