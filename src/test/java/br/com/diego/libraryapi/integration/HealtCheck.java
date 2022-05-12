9fcd1c88f39a7d0ef5afe942f4c4d9b8484a9596
Diego Rocha <di.rocha27@gmail.com>
2022-05-12 00:32:35 -0300

    chore: add test healtCheck

--- src/test/java/br/com/diego/libraryapi/integration/HealtCheck.java
+++
@@ -1,7 +0,0 @@
-package br.com.diego.libraryapi.integration;
-
-import br.com.diego.libraryapi.integration.config.BaseAPI;
-
-public class HealtCheck extends BaseAPI {
-    //TODO
-}
---
+++ src/test/java/br/com/diego/libraryapi/integration/HealtCheckTest.java
@@ -0,0 +1,35 @@
+package br.com.diego.libraryapi.integration;
+
+import br.com.diego.libraryapi.LibraryApiApplication;
+import br.com.diego.libraryapi.integration.config.BaseAPI;
+import io.restassured.builder.RequestSpecBuilder;
+import io.restassured.specification.RequestSpecification;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+
+import static io.restassured.RestAssured.basePath;
+import static io.restassured.RestAssured.given;
+@ActiveProfiles("integration")
+@SpringBootTest(classes = LibraryApiApplication.class, webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
+class HealtCheckTest extends BaseAPI {
+    @Test
+    @DisplayName("devo retornar 200 - healthCheck")
+    void healthCheckReturn200() {
+        basePath = "/actuator/health";
+
+        RequestSpecification requestSpec = new RequestSpecBuilder()
+                .setContentType("application/json")
+                .setAccept("*/*")
+                .build();
+
+        given().log().all()
+                .spec(requestSpec)
+                .get()
+                .then()
+                    .assertThat()
+                    .statusCode(200);
+    }
+
+}
